75dec27f747a0cdbb070df15a8d3dfe15e0a2ab1
Silent Web Studio <307634623+silentwebstudio@users.noreply.github.com>
2026-07-23 19:11:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ window.addEventListener("scroll", () => {
         header.style.background = "rgba(0,0,0,0.7)";
     }
 });
+
+// Back To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 400){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
